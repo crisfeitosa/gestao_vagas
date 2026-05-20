4552136c374cc3cff36b16b2f18dfc8bb96f8a2b
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-20 17:21:50 -0300

    Initialize project with Maven setup, including wrapper scripts, POM file, and basic application structure

---
+++ src/test/java/br/com/cristianofeitosa/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cristianofeitosa.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
