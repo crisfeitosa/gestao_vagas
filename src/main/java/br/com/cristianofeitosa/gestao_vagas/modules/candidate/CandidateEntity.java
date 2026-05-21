5450847e7fdd4fee81f1c51a2ddddbb621b2b9e7
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-21 11:36:09 -0300

    Add validation dependencies and enhance CandidateEntity and CandidateController for input validation

--- src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.cristianofeitosa.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O nome de usuário deve conter apenas letras, números, pontos, sublinhados ou hífens")
   private String username;
+
+  @Email(message = "Email deve ser válido")
   private String email;
+
+  @Length(min = 10, max = 20, message = "A senha deve conter entre 10 e 20 caracteres")
   private String password;
   private String description;
   private String curriculum;
