5b1ca13f82c85cdb2711d215be132bd77e57fb00
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-26 10:29:07 -0300

    Update password validation and encoding in CandidateEntity and CreateCandidateUseCase

--- src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -32,7 +32,7 @@ public class CandidateEntity {
   @Email(message = "Email deve ser válido")
   private String email;
 
-  @Length(min = 10, max = 20, message = "A senha deve conter entre 10 e 20 caracteres")
+  @Length(min = 10, message = "A senha deve conter pelo menos (10) caracteres")
   private String password;
   private String description;
   private String curriculum;
