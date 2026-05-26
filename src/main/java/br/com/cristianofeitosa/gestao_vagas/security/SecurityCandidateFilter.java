b6b0c5ef024eadeee8118f7bf53ac6dd63bc51ae
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-26 15:12:02 -0300

    Refactor authentication endpoints and update security filters for company and candidate

--- src/main/java/br/com/cristianofeitosa/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/cristianofeitosa/gestao_vagas/security/SecurityCandidateFilter.java
@@ -22,19 +22,22 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-
     SecurityContextHolder.getContext().setAuthentication(null);
     String header = request.getHeader("Authorization");
 
-    if (header != null) {
-      var token = this.jwtProvider.validateToken(header);
+    if (request.getRequestURI().startsWith("/candidate")) {
+      if (header != null) {
+        var token = this.jwtProvider.validateToken(header);
 
-      if (token == null) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return;
+        if (token == null) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+
+        request.setAttribute("candidate_id", token.getSubject());
+        var roles = token.getClaim("roles");
       }
 
-      request.setAttribute("candidate_id", token.getSubject());
     }
 
     filterChain.doFilter(request, response);
