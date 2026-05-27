b964f40c640f0e0e28269018638b4b93a9b9729d
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-27 09:53:45 -0300

    Add Swagger UI dependencies and update security configuration to permit access to API documentation

--- src/main/java/br/com/cristianofeitosa/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/cristianofeitosa/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.cristianofeitosa.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas API", version = "1.0", description = "API para gestão de vagas de emprego, permitindo que empresas criem e gerenciem suas vagas, e candidatos se inscrevam nas oportunidades disponíveis."))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
