4d9f95dfb406f8205411a1ef49428f23c0ffd23b
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-27 10:56:14 -0300

    Add job filtering functionality to CandidateController and implement ListAllJobsByFilterUseCase

--- src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/cristianofeitosa/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.cristianofeitosa.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.cristianofeitosa.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.cristianofeitosa.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.cristianofeitosa.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.cristianofeitosa.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -27,6 +31,9 @@ public class CandidateController {
 
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -49,5 +56,11 @@ public class CandidateController {
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @GetMapping("/jobs")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
